084979c0ca4660b3c99c0bf20d04a0cd68478368
chenyongchu <obeeycyc@gmail.com>
2020-09-29 14:36:00 +0800

    Add test case: buy_eth_more_hbtc_from_market

--- Hedging/src/test/java/io/cofix/hedging/service/HedgingJobTests.java
+++ Hedging/src/test/java/io/cofix/hedging/service/HedgingJobTests.java
@@ -85,6 +85,52 @@ public class HedgingJobTests {
 
     /**
      * 初始ETH为1eth，erc20为1，eth:erc20=1:3
+     * 变化为0.5eth, erc20为2.2.变化量为少了0.5eth,多了1.2erc20
+     * 此时应该在交易市场买入0.4eth,卖出1.2erc20
+     */
+    @Test
+    public void buy_eth_more_hbtc_from_market() {
+        BigDecimal ethThreshold = new BigDecimal("0.0001");
+        BigDecimal erc20Threshold = new BigDecimal("0.0001");
+        Mockito.when(mockHedgingPoolService.getExchangePrice()).thenReturn(BigDecimal.valueOf(3));
+        Mockito.when(mockHedgingPoolService.getBalance()).thenReturn(BigInteger.valueOf(1));
+        Mockito.when(mockHedgingPoolService.getTotalSupply()).thenReturn(BigInteger.valueOf(100));
+        Mockito.when(mockHedgingPoolService.getEth()).thenReturn(new BigInteger("1000000000000000000"));
+        Mockito.when(mockHedgingPoolService.getErc20()).thenReturn(new BigInteger("1000000000000000000"));
+        Mockito.when(mockHedgingPoolService.getDecimals()).thenReturn(BigInteger.valueOf(18));
+        Mockito.when(mockHedgingPoolService.getEthThreshold()).thenReturn(ethThreshold);
+//        Mockito.when(mockHedgingPoolService.getErc20Threshold()).thenReturn(erc20Threshold);
+        Mockito.when(mockHedgingPoolService.getSymbol()).thenReturn("ethbtc");
+
+        hedgingJob.hedgingPool(mockHedgingPoolService, mockTradeMarketService);
+        verify(mockTradeMarketService, never()).sendBuyMarketOrder(anyString(), anyString());
+        verify(mockTradeMarketService, never()).sendSellMarketOrder(anyString(), anyString());
+        verify(mockHedgingPoolService, times(1)).setOldPoolAmountVo(any());
+
+        Mockito.when(mockHedgingPoolService.getBalance()).thenReturn(BigInteger.valueOf(1));
+        Mockito.when(mockHedgingPoolService.getTotalSupply()).thenReturn(BigInteger.valueOf(100));
+        Mockito.when(mockHedgingPoolService.getEth()).thenReturn(new BigInteger("500000000000000000"));
+        Mockito.when(mockHedgingPoolService.getErc20()).thenReturn(new BigInteger("2700000000000000000"));
+        Mockito.when(mockHedgingPoolService.getDecimals()).thenReturn(BigInteger.valueOf(18));
+        Mockito.when(mockHedgingPoolService.getDeltaEth()).thenReturn(new BigDecimal("-500000000000000000"));
+        Mockito.when(mockHedgingPoolService.getDeltaErc20()).thenReturn(new BigDecimal("1200000000000000000"));
+        Mockito.when(mockHedgingPoolService.getOldPoolAmountVo()).thenReturn(
+                new PoolAmountVo(BigInteger.valueOf(1),
+                        BigInteger.valueOf(100),
+                        new BigInteger("1000000000000000000"),
+                        new BigInteger("1000000000000000000"),
+                        BigInteger.valueOf(18)));
+        Order   order   = new Order();
+        order.setState("filled");
+        Mockito.when(mockTradeMarketService.getOrderById(anyLong())).thenReturn(order);
+        hedgingJob.hedgingPool(mockHedgingPoolService, mockTradeMarketService);
+
+        verify(mockTradeMarketService, times(1)).sendBuyMarketOrder(eq(mockHedgingPoolService.getSymbol()), eq("1.200000000000000000"));
+        verify(mockTradeMarketService, never()).sendSellMarketOrder(anyString(), anyString());
+    }
+
+    /**
+     * 初始ETH为1eth，erc20为1，eth:erc20=1:3
      * 变化为0.5eth, erc20为2.7.变化量为少了0.5eth,多了1.7erc20
      * 此时应该在交易市场买入0.5eth,卖出1.5erc20
      */
